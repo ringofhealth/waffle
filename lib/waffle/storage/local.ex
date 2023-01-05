593e6dc14a46841837f66a348d28ae4c7dcef203
Your name here <your_email@example.com>
2023-01-04 20:49:19 -0500

    running formatter

--- lib/waffle/storage/local.ex
+++ lib/waffle/storage/local.ex
@@ -25,11 +25,13 @@ defmodule Waffle.Storage.Local do
   alias Waffle.Definition.Versioning
 
   def put(definition, version, {file, scope}) do
-    destination_path = Path.join([
-      definition.storage_dir_prefix(),
-      definition.storage_dir(version, {file, scope}),
-      file.file_name
-    ])
+    destination_path =
+      Path.join([
+        definition.storage_dir_prefix(),
+        definition.storage_dir(version, {file, scope}),
+        file.file_name
+      ])
+
     destination_path |> Path.dirname() |> File.mkdir_p!()
 
     if binary = file.binary do
@@ -42,10 +44,12 @@ defmodule Waffle.Storage.Local do
   end
 
   def url(definition, version, file_and_scope, _options \\ []) do
-    local_path = Path.join([
-      definition.storage_dir(version, file_and_scope),
-      Versioning.resolve_file_name(definition, version, file_and_scope)
-    ])
+    local_path =
+      Path.join([
+        definition.storage_dir(version, file_and_scope),
+        Versioning.resolve_file_name(definition, version, file_and_scope)
+      ])
+
     host = host(definition)
 
     if host == nil do
